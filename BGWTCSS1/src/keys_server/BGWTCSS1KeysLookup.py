ee2f3fc5b25c073d77175dc161f9836ef655d64b
KamalCharles <kamal.charles@oasispalmtree.co.uk>
2021-01-26 14:20:51 +0000

    Bangladesh-Cyclone-BGWTCSS1

---
+++ BGWTCSS1/src/keys_server/BGWTCSS1KeysLookup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'BGWTCSS1KeysLookup'
+]
+
+from keys_server import CatrisksBaseKeysLookup 
+
+from oasislmf.utils.log import oasis_log
+
+
+class BGWTCSS1KeysLookup(CatrisksBaseKeysLookup):
+    """
+    CatRisks MENAEQ model keys lookup logic - at present the MENAEQ lookup logic
+    is identical to that of the Catrisks generic keys lookup, but future
+    differences can be dealt with by overriding the relevant generic methods here.
+    """
+
+    @oasis_log()
+    def __init__(self, keys_data_directory=None, supplier='UKMO', model_name='BGWTCSS1', model_version=None):
+        super(self.__class__, self).__init__(keys_data_directory, supplier, model_name, model_version)
---
+++ BGWTCSS1/src/keys_server/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package initialiser.
+"""
+
+from .CatrisksBaseKeysLookup import *
+from .BGWTCSS1KeysLookup import *
+from .common_utils import *
+
+
